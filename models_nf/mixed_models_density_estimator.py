6781ddb30d05e743469ae57111d002b46566e314
Elouan <elouan.argouarch@gmail.com>
2022-06-30 12:10:35 +0200

    number params

--- models_nf/mixed_models_density_estimator.py
+++ models_nf/mixed_models_density_estimator.py
@@ -18,6 +18,12 @@ class MixedModelDensityEstimator(nn.Module):
             self.model.insert(0, structure[i][0](self.p, structure[i][1], q_log_density=self.model[0].log_psi))
         self.loss_values= []
 
+    def compute_number_params(self):
+        number_params = 0
+        for model in self.model:
+            number_params += sum(p.numel() for p in model.parameters() if p.requires_grad)
+        return number_params
+
     def sample_model(self, num_samples):
         with torch.no_grad():
             z = self.reference.sample(num_samples)

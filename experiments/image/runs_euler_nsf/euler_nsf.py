07f1fd8f444ea97a0c876af6a87316ee9acb3be2
EA264728 <elouan.argouarch@cea.fr>
2022-07-08 16:01:21 +0200

    ready for simu nsf

--- experiments/image/runs_euler_nsf/euler_nsf.py
+++ experiments/image/runs_euler_nsf/euler_nsf.py
@@ -31,20 +31,4 @@ for i in range(number_runs):
     nsf.train(epochs, batch_size)
 
     filename = 'runs_euler_nsf' + str(i) + '.sav'
-    #torch.save(nsf, filename)
-    with torch.no_grad():
-        grid = torch.cartesian_prod(torch.linspace(0,1,lines),torch.linspace(0,1, columns))
-        density = torch.exp(nsf.model.log_prob(grid)).reshape(lines,columns).T
-        figure = plt.figure(figsize = (12,8))
-        ax = figure.add_subplot(121)
-        ax.tick_params(left=False,bottom=False,labelleft=False,labelbottom=False)
-        ax.imshow(torch.flip(torch.flip(density.T,[0,1]),[0,1]),extent = [0,columns,0, lines])
-        ax = figure.add_subplot(122)
-        ax.tick_params(left=False,bottom=False,labelleft=False,labelbottom=False)
-        ax.imshow(torch.flip(torch.flip(density.T,[0,1]),[0,1]),extent = [0,columns,0, lines])
-    figure.savefig('runs_euler_nsf' + str(i) + '.png')
-    loss_values.append(torch.tensor([nsf.loss_values[-1]]))
-with open('readme.txt', 'w') as f:
-    f.write('mean =' + str(torch.mean(torch.cat(loss_values))))
-    f.write('\n')
-    f.write('std =' + str(torch.std(torch.cat(loss_values))))
+    torch.save(nsf.state_dict(), filename)

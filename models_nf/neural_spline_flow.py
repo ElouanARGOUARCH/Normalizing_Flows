64c516592928e67dc62fef8d13888feff6970b79
EA264728 <elouan.argouarch@cea.fr>
2022-07-08 10:26:44 +0200

    added neural spline flow

--- models_nf/neural_spline_flow.py
+++ models_nf/neural_spline_flow.py
@@ -1,24 +1,19 @@
 import torch
 from torch import nn
-import numpy as np
 import normflow as nf
-
-from sklearn.datasets import make_moons
-
-from matplotlib import pyplot as plt
 
 from tqdm import tqdm
 
 class NeuralSplineFlow(nn.Module):
     def __init__(self, target_samples,K, hidden_units, hidden_layers):
-        super.__init__(self)
+        super().__init__()
         self.target_samples = target_samples
         self.K = K
         self.hidden_units = hidden_units
         self.hidden_layers = hidden_layers
         self.p = target_samples.shape[-1]
         flows = []
-        for i in range(K):
+        for i in range(self.K):
             flows += [nf.flows.AutoregressiveRationalQuadraticSpline(self.p, hidden_layers,hidden_units)]
             flows += [nf.flows.LULinearPermute(self.p)]
 
@@ -26,7 +21,11 @@ class NeuralSplineFlow(nn.Module):
         self.q0 = nf.distributions.DiagGaussian(2, trainable=False)
 
         # Construct flow model
-        self.model = nf.NormalizingFlow(q0=q0, flows=flows)
+        self.model = nf.NormalizingFlow(q0=self.q0, flows=flows)
+        self.loss_values = []
+
+    def compute_number_params(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
 
     def train(self, epochs, batch_size = None):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -50,8 +49,5 @@ class NeuralSplineFlow(nn.Module):
             with torch.no_grad():
                 iteration_loss = torch.tensor([self.model.forward_kld(batch[0].to(device)) for i, batch in enumerate(dataloader)]).mean().item()
             self.loss_values.append(iteration_loss)
-            pbar.set_postfix_str('loss = ' + str(round(iteration_loss,6)) + ' ; device: ' + str(self.model.device))
-
+            pbar.set_postfix_str('loss = ' + str(round(iteration_loss,6)) + ' ; device: ' + str(device))
         self.to('cpu')
-        for model in self.model:
-            model.to(torch.device('cpu'))

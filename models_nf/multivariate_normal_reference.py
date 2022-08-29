27eb7159d1bdaf99104d55a3b871d2539f9d42e6
EA264728 <elouan.argouarch@cea.fr>
2022-08-29 11:24:55 +0200

    changed base distribution

--- models_nf/multivariate_normal_reference.py
+++ models_nf/multivariate_normal_reference.py
@@ -11,11 +11,12 @@ class MultivariateNormalReference(nn.Module):
 
     def estimate_moments(self, samples):
         self.mean = torch.mean(samples, dim = 0)
-        self.cov = torch.cov(samples.T)
+        self.cov = torch.cov(samples.T) + 1e-8*torch.eye(self.p)
         self.distribution = torch.distributions.MultivariateNormal(self.mean, self.cov)
 
     def sample(self, num_samples):
         return self.distribution.sample([num_samples])
+        #return torch.randn([num_samples, self.p])
 
     def log_density(self, z):
         return self.distribution.log_prob(z)
